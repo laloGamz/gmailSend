d3b2f314c5f0276ad84d808763d06d1940ca0bd6
guilleguzman <guillermoguzman.2016@gmail.com>
2021-09-11 15:50:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 const Telegraf = require('telegraf');
-const Markup = require("telegraf/markup");
-const Stage = require("telegraf/stage");
-const session = require("telegraf/session");
-const WizardScene = require("telegraf/scenes/wizard");
+const session = require('telegraf/session');
+const Stage = require('telegraf/stage');
+const WizardScene = require('telegraf/scenes/wizard');
 
-var nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const bot = new Telegraf('1969516967:AAFPXAcbSn3pZHCfcE3MD6rfyMq-sLvLgIA')
+const superWizard = new WizardScene(
+  'super-wizard',
+  ctx => {
+    ctx.reply("Ingrese el mail de destino?");
+    ctx.wizard.state.data = {};
+    return ctx.wizard.next();
+  },
+  ctx => {
+    ctx.wizard.state.data.name = ctx.message.text;
+    ctx.reply('Ingrese su mensaje');
+    return ctx.wizard.next();
+  },
+  ctx => {
+    ctx.wizard.state.data.phone = ctx.message.text;
 
-bot.start(ctx => {
-  ctx.reply(
-    `Hola ${ctx.from.first_name}, como estas???, este es un bot para enviar email`,
-    Markup.inlineKeyboard([
-      Markup.callbackButton("enviar Mail", "LOVE_CALCULATE"),
-      { text: 'Ir a Habilitar gmail como aplicacion segura', url: 'google.com' }
-    ]).extra()
-  );
-});
+    var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: 'guilleamazon.2016@gmail.com',
+          pass: '153221081'
+      }
+    });
 
+    var mailOptions = {
+      from: 'guillermoguzman.2016@gmail.com',
+      to: ctx.wizard.state.data.name,
+      subject: 'Asunto',
+      text: ctx.wizard.state.data.phone
+    };
 
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error){
+          console.log(error);
+          //res.send(500, err.message);
+      } else {
+          console.log("Email sent");
+          ctx.reply('mensaje enviado');
+          //res.status(200).jsonp(req.body);
+      }
+    })
 
-const stage = new Stage([loveCalculate], { default: "love_calculate" });
+    return ctx.scene.leave();
+  }
+);
+const stage = new Stage([superWizard]);
+
+const bot = new Telegraf('1969516967:AAFPXAcbSn3pZHCfcE3MD6rfyMq-sLvLgIA');
 bot.use(session());
 bot.use(stage.middleware());
+bot.command('enviar', ctx => {
+  ctx.scene.enter('super-wizard');
+});
 bot.launch();
