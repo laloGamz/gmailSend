5132a808e09afe15140dffc0dd0ae96b433d9e8d
guilleguzman <guillermoguzman.2016@gmail.com>
2021-09-23 14:47:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Stage = require('telegraf/stage');
 const WizardScene = require('telegraf/scenes/wizard');
 const mysql = require('mysql');
 const nodemailer = require("nodemailer");
+const util = require('util');
 
 var con = mysql.createConnection({
   host: "185.201.11.128",
@@ -32,50 +33,56 @@ const superWizard = new WizardScene(
     return ctx.wizard.next();
   },
   ctx => {
+    
     ctx.wizard.state.data.phone = ctx.message.text;
     
-    const mails =['golondrinasient@gmail.com','axonzte58@gmail.com','greciatonally@gmail.com','aaronpinzon30q@gmail.com','golondrina202221@gmail.com','fuegocruzado2020@gmail.com','plugin8080@gmail.com','plugin252525@gmail.com','guilleamazon.2016@gmail.com','mexicotierrahackers2020@gmail.com','musicaalairelibre2020@gmail.com','greciatonally@gmail.com','sheinofertas525@gmail.com','amazonprime20u@gmail.com','chedraguicuestamenos@gmail.com','negociosonline17u@gmail.com','sheinhistorial@gmail.com','coppelrenueva@gmail.com'];
+    const query = util.promisify(conn.query).bind(conn);
+
+    var rows = '' ;
+
+    (async () => {
+      try {
+        rows = await query('SELECT * FROM user WHERE llave = "${ctx.wizard.state.data.key}"');
+        //console.log(rows.length);
+      } finally {
+        conn.end();
+      }
+    })()
     
-   con.connect(function(err) {
-      if (err) throw err;
-     
-      con.query('SELECT * FROM user WHERE status = "${ctx.wizard.state.data.key}"', function (err, result, fields) {
+    if(rows.length != 0){
+
+      const mails =['golondrinasient@gmail.com','axonzte58@gmail.com','greciatonally@gmail.com','aaronpinzon30q@gmail.com','golondrina202221@gmail.com','fuegocruzado2020@gmail.com','plugin8080@gmail.com','plugin252525@gmail.com','guilleamazon.2016@gmail.com','mexicotierrahackers2020@gmail.com','musicaalairelibre2020@gmail.com','greciatonally@gmail.com','sheinofertas525@gmail.com','amazonprime20u@gmail.com','chedraguicuestamenos@gmail.com','negociosonline17u@gmail.com','sheinhistorial@gmail.com','coppelrenueva@gmail.com'];
+    
+      mails.forEach(function myFunction(value) {
         
-        if (err) return ctx.scene.leave();
-        
-        mails.forEach(function myFunction(value) {
-      
-          var transporter = nodemailer.createTransport({
+        var transporter = nodemailer.createTransport({
           service: 'Gmail',
           auth: {
               user: value,
               pass: 'Alor_1130'
               }
           });
-
+        
         var mailOptions = {
           from: value,
           to: ctx.wizard.state.data.name,
           subject: 'Asunto',
           text: ctx.wizard.state.data.phone
         };
-     
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error){
-          console.log(error);
-          //res.send(500, err.message);
-      } else {
-          console.log("Email sent");
-          ctx.reply('mensaje enviado');
-          //res.status(200).jsonp(req.body);
-      }
+        
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error){
+            console.log(error);
+            //res.send(500, err.message);
+        } else {
+            console.log("Email sent");
+            ctx.reply('mensaje enviado');
+            //res.status(200).jsonp(req.body);
+        }
     })
-    
+      
     });
-      });
-  });
-
+    }
     
 
     return ctx.scene.leave();
