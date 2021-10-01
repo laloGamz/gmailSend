66a607ec9f2b7fc8daacecbd331cb314c1347932
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-01 03:23:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -112,21 +112,7 @@ const stage = new Stage([superWizard]);
 const bot = new Telegraf('1969516967:AAFPXAcbSn3pZHCfcE3MD6rfyMq-sLvLgIA');
 bot.use(session());
 bot.use(stage.middleware());
-bot.command('info', (ctx) => {
-  
-  const query = util.promisify(con.query).bind(con);
-  
-  try {
-    var info = await query(`SELECT * FROM user WHERE first_name ="${ctx.form.username}"`);
-  
-    ctx.reply(info[0].llave);
-    ctx.reply(info[0].credito);
-  }
-  finally {
-        con.end();
-  }
-  
-});
+
 bot.command('nombre', (ctx) => ctx.reply(ctx.from.username));
 bot.command('enviar', ctx => {
 
