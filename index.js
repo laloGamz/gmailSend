3f50210e0cbf02a6db05a952608806734fff2ca2
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-01 17:36:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const mysql = require('mysql');
 const nodemailer = require("nodemailer");
 const util = require('util');
 
-var con = mysql.createConnection({
-        host: "185.201.11.128",
-        user: "u270568211_pablod",
-        password: "Guillermo2020.",
-        database: "u270568211_juegosgamer"
-      });
+
 
 const bot = new Telegraf('1969516967:AAFPXAcbSn3pZHCfcE3MD6rfyMq-sLvLgIA');
 bot.use(session());
 //bot.use(stage.middleware());
 bot.command('info', (ctx) => {
+        
+ var con = mysql.createConnection({
+        host: "185.201.11.128",
+        user: "u270568211_pablod",
+        password: "Guillermo2020.",
+        database: "u270568211_juegosgamer"
+      });
  
        
   const query = util.promisify(con.query).bind(con);
