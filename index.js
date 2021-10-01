5f45799575d060ae6a36a867dfe3c1b97ee65484
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-01 17:05:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,8 @@ bot.command('info', (ctx) => {
   (async () => {
           
           try{
-                  const username = ctx.from.username;
                   
-                  const info = await query(`SELECT * FROM user WHERE first_name =${username}`);
+                  const info = await query(`SELECT * FROM user WHERE first_name ="${ctx.from.username}"`);
   
                   console.log(info[0].llave);
                   ctx.reply(info[0].llave);
