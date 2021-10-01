790258525d22f38fb7f536dc3b27ce3dde863387
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-01 16:03:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,102 +13,6 @@ var con = mysql.createConnection({
         database: "u270568211_juegosgamer"
       });
 
-
-const superWizard = new WizardScene(
-  'super-wizard',
-  ctx => {
-    ctx.reply("Ingrese su llave");
-    ctx.wizard.state.data = {};
-    return ctx.wizard.next();
-  },
-  ctx => {
-    ctx.reply("Ingrese el mail de destino?");
-    ctx.wizard.state.data.key = ctx.message.text;
-    return ctx.wizard.next();
-  },
-  ctx => {
-    ctx.wizard.state.data.email = ctx.message.text;
-    ctx.reply('Ingrese su mensaje');
-    return ctx.wizard.next();
-  },
-  ctx => {
-    
-    //poner de nuevo el con
-    
-    ctx.wizard.state.data.mensaje = ctx.message.text;
-    
-    const query = util.promisify(con.query).bind(con);
-
-    var rows = '' ;
-    
-    var rows2 = '' ;
-
-    (async () => {
-      try {
-        rows = await query(`SELECT * FROM user WHERE llave = "${ctx.wizard.state.data.key}"`);
-
-        console.log(rows.length);
-        
-        rows2 = await query(`SELECT credito FROM user WHERE llave ="${ctx.wizard.state.data.key}"`);
-        
-        var creditos = rows2[0].credito;
-
-        if(creditos>0){
-
-            if(rows.length !=0){
-
-                rows2 = await query(`UPDATE user SET credito=${creditos -1} WHERE llave="${ctx.wizard.state.data.key}"`);
-
-                const mails =['golondrinasient@gmail.com','axonzte58@gmail.com','greciatonally@gmail.com','aaronpinzon30q@gmail.com','golondrina202221@gmail.com','fuegocruzado2020@gmail.com','plugin8080@gmail.com','plugin252525@gmail.com','guilleamazon.2016@gmail.com','mexicotierrahackers2020@gmail.com','musicaalairelibre2020@gmail.com','greciatonally@gmail.com','sheinofertas525@gmail.com','amazonprime20u@gmail.com','chedraguicuestamenos@gmail.com','negociosonline17u@gmail.com','sheinhistorial@gmail.com','coppelrenueva@gmail.com'];
-    
-                mails.forEach(function myFunction(value) {
-            
-                    var transporter = nodemailer.createTransport({
-                      service: 'Gmail',
-                      auth: {
-                          user: value,
-                          pass: 'Alor_1130'
-                          }
-                      });
-                    
-                    var mailOptions = {
-                      from: value,
-                      to: ctx.wizard.state.data.email,
-                      subject: 'Asunto',
-                      text: ctx.wizard.state.data.mensaje
-                    };
-                    
-                  transporter.sendMail(mailOptions, function(error, info){
-                    if (error){
-                        console.log(error);
-                        //res.send(500, err.message);
-                    } else {
-                        console.log("Email sent");
-                        ctx.reply('mensaje enviado');
-                        //res.status(200).jsonp(req.body);
-                    }
-                })
-                  
-                });
-            }
-
-
-        }
-
-        
-
-      } finally {
-        con.end();
-        return ctx.scene.leave();
-      }
-    })()
-    
-
-  }
-);
-
-const stage = new Stage([superWizard]);
-
 const bot = new Telegraf('1969516967:AAFPXAcbSn3pZHCfcE3MD6rfyMq-sLvLgIA');
 bot.use(session());
 bot.use(stage.middleware());
@@ -116,12 +20,12 @@ bot.command('info', (ctx) => {
   
   var username = ctx.from.username;
        
-  const query2 = util.promisify(con.query).bind(con);
+  const query = util.promisify(con.query).bind(con);
  
         
   (async () => {
         try {
-    var info = await query2(`SELECT * FROM user WHERE first_name ="${username}"`);
+    const info = await query(`SELECT * FROM user WHERE first_name ="${username}"`);
   
     console.log(info[0].llave);
   }
