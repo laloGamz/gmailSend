e3b5c3b93ddcd515eea5c293eb19f921d5cfbe85
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-01 01:29:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,46 +45,56 @@ const superWizard = new WizardScene(
     (async () => {
       try {
         rows = await query(`SELECT * FROM user WHERE llave = "${ctx.wizard.state.data.key}"`);
+
         console.log(rows.length);
         
         rows2 = await query(`SELECT credito FROM user WHERE llave ="${ctx.wizard.state.data.key}"`);
         
-        console.log(rows2[0].credito);
+        var creditos = rows2[0].credito;
 
-        if(rows.length !=0){
+        if(creditos>0){
 
-            const mails =['golondrinasient@gmail.com','axonzte58@gmail.com','greciatonally@gmail.com','aaronpinzon30q@gmail.com','golondrina202221@gmail.com','fuegocruzado2020@gmail.com','plugin8080@gmail.com','plugin252525@gmail.com','guilleamazon.2016@gmail.com','mexicotierrahackers2020@gmail.com','musicaalairelibre2020@gmail.com','greciatonally@gmail.com','sheinofertas525@gmail.com','amazonprime20u@gmail.com','chedraguicuestamenos@gmail.com','negociosonline17u@gmail.com','sheinhistorial@gmail.com','coppelrenueva@gmail.com'];
+            if(rows.length !=0){
 
-            mails.forEach(function myFunction(value) {
-        
-                var transporter = nodemailer.createTransport({
-                  service: 'Gmail',
-                  auth: {
-                      user: value,
-                      pass: 'Alor_1130'
-                      }
-                  });
-                
-                var mailOptions = {
-                  from: value,
-                  to: ctx.wizard.state.data.email,
-                  subject: 'Asunto',
-                  text: ctx.wizard.state.data.mensaje
-                };
-                
-              transporter.sendMail(mailOptions, function(error, info){
-                if (error){
-                    console.log(error);
-                    //res.send(500, err.message);
-                } else {
-                    console.log("Email sent");
-                    ctx.reply('mensaje enviado');
-                    //res.status(200).jsonp(req.body);
-                }
-            })
-              
-            });
+                rows2 = await query(`UPDATE user SET credito=${creditos -1} WHERE llave="${ctx.wizard.state.data.key}"`);
+
+                const mails =['golondrinasient@gmail.com','axonzte58@gmail.com','greciatonally@gmail.com','aaronpinzon30q@gmail.com','golondrina202221@gmail.com','fuegocruzado2020@gmail.com','plugin8080@gmail.com','plugin252525@gmail.com','guilleamazon.2016@gmail.com','mexicotierrahackers2020@gmail.com','musicaalairelibre2020@gmail.com','greciatonally@gmail.com','sheinofertas525@gmail.com','amazonprime20u@gmail.com','chedraguicuestamenos@gmail.com','negociosonline17u@gmail.com','sheinhistorial@gmail.com','coppelrenueva@gmail.com'];
+    
+                mails.forEach(function myFunction(value) {
+            
+                    var transporter = nodemailer.createTransport({
+                      service: 'Gmail',
+                      auth: {
+                          user: value,
+                          pass: 'Alor_1130'
+                          }
+                      });
+                    
+                    var mailOptions = {
+                      from: value,
+                      to: ctx.wizard.state.data.email,
+                      subject: 'Asunto',
+                      text: ctx.wizard.state.data.mensaje
+                    };
+                    
+                  transporter.sendMail(mailOptions, function(error, info){
+                    if (error){
+                        console.log(error);
+                        //res.send(500, err.message);
+                    } else {
+                        console.log("Email sent");
+                        ctx.reply('mensaje enviado');
+                        //res.status(200).jsonp(req.body);
+                    }
+                })
+                  
+                });
+            }
+
+
         }
+
+        
 
       } finally {
         con.end();
